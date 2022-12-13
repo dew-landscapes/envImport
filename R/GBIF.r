719e0ff462b50949724a0b4eadd12816c0ec2437
Acanthiza <nigel.willoughby@sa.gov.au>
2022-12-13 14:48:39 +1030

    Simplified `get_GBIF`

--- R/GBIF.r
+++ R/GBIF.r
@@ -1,5 +1,5 @@
 
-#' Get occurrence records from GBIF
+#' Get new occurrence record set from GBIF
 #'
 #' GBIF is the
 #' [Global Biodiversity Information Facility](https://www.gbif.org/).
@@ -25,6 +25,9 @@
 #' @param out_file Character. Path to save output data.
 #' @param data_map Dataframe. Mapping of GBIF fields to retrieve and their new
 #' names
+#' @param wait_time Time to wait between running `rgbif::occ_download_meta` to
+#' check status. Will keep trying while status == "RUNNING".
+#'
 #'
 #' @return If `out_file` is supplied: dataframe, `rio::export(results, out_file)`,
 #' and `gbif_data_ref.bib` (in the same directory as `out_file`); else dataframe.
@@ -36,13 +39,16 @@
 #' # but can take a while to run depending on GBIF waiting times.
 #' get_GBIF(taxon_key = 2474903, rgbif::pred("year", 1901))
 #'}
-  get_GBIF <- function(taxon_key = 1
+  get_GBIF <- function(taxon_key
                        , ...
                        , poly = NULL
                        , poly_buf = 100000
                        , out_file = NULL
                        , data_map = NULL
+                       , wait_time = 10
                        ) {
+
+    if(wait_time < 10) wait_time = 10
 
     if(!is.null(poly)) {
 
@@ -62,12 +68,42 @@
                                 , rgbif::pred("hasCoordinate"
                                               , TRUE
                                               )
-                                , ...
+                                , if(!is.null(poly)) rgbif::pred_within(aoiWKT)
+                                #, ...
                                 )
 
-    rgbif::occ_download_wait(temp)
-
     meta <- rgbif::occ_download_meta(temp)
+
+    attempt <- 1
+
+    start_time <- Sys.time()
+
+    while(meta$status %in% c("PREPARING", "RUNNING")) {
+
+      cat(paste0("Status = ", meta$status, "\n"
+                 , "Attempt = ", attempt, "\n"
+                 , "Each wait time = ", wait_time, " seconds\n"
+                 , "Total time = ", Sys.time() - start_time
+                 )
+          )
+
+      Sys.sleep(wait_time)
+
+      meta <- rgbif::occ_download_meta(temp)
+
+      attempt <- attempt + 1
+
+    }
+
+    if(meta$status != "SUCCEEDED") {
+
+      print(meta)
+
+      stop("Status not equal to 'SUCCEEDED'")
+
+    }
+
+
 
     if(!is.null(out_file)) {
 
@@ -90,74 +126,62 @@
                                            , overwrite = TRUE
                                            )
 
-    #------get gbif meta data---------
 
-    info <- rgbif::occ_download_list(Sys.getenv("GBIF_user")
-                                     , Sys.getenv("GBIF_pwd")
-                                     )$results %>%
-      dplyr::mutate(created = lubridate::ymd_hms(created
-                                                 , tz = Sys.timezone()
-                                                 )
-                    , taxon_key = purrr::map(request.predicate.predicates
-                                             , c("value")
-                                             )
-                    , taxon_key = purrr::map(taxon_key
-                                             , 1
-                                             )
-                    ) %>%
-      tidyr::unnest(cols = c(taxon_key)) %>%
-      dplyr::filter(taxon_key == taxon_key
-                    , created == max(created)
-                    )
+    # MOVE THIS SECTION TO NEW FUNCTION TO RETRIEVE A DOWNLOAD VIA KEY
+    if(FALSE) {
 
-    #------metaGBIF-------
+      #------get gbif meta data---------
 
-    if(!is.null(out_file)) {
-
-      metaGBIF <- list()
-
-      metaGBIF$key <- info %>%
-        dplyr::pull(key)
-
-      metaGBIF$doi <- info %>%
-        dplyr::pull(doi)
-
-      metaGBIF$licence <- info %>%
-        dplyr::pull(license)
-
-      metaGBIF$date <- info %>%
-        dplyr::pull(created)
-
-
-      bib_file <- fs::path(save_loc, "gbif_data_ref.bib")
-
-      metaGBIF$ref <- RefManageR::GetBibEntryWithDOI(metaGBIF$doi
-                                                     , temp.file = bib_file
-                                                     , delete.file = FALSE
-                                                     )
-
-      # Make a reference for the download
-      ref <- readr::read_lines(bib_file)
-      ref[1] <- paste0("@misc{GBIFRef,")
-      readr::write_lines(ref, bib_file)
+      info <- rgbif::occ_download_list(Sys.getenv("GBIF_user")
+                                       , Sys.getenv("GBIF_pwd")
+                                       )$results %>%
+        dplyr::mutate(created = lubridate::ymd_hms(created
+                                                   , tz = Sys.timezone()
+                                                   )
+                      , taxon_key = purrr::map(request.predicate.predicates
+                                               , c("value")
+                                               )
+                      , taxon_key = purrr::map(taxon_key
+                                               , 1
+                                               )
+                      ) %>%
+        tidyr::unnest(cols = c(taxon_key)) %>%
+        dplyr::filter(taxon_key == taxon_key
+                      , created == max(created)
+                      )
 
     }
+
+    #------bib-------
+
+    bib_file <- fs::path(save_loc, "gbif_data_ref.bib")
+
+    ref <- RefManageR::GetBibEntryWithDOI(meta$doi
+                                          , temp.file = bib_file
+                                          , delete.file = FALSE
+                                          )
+
+    # Make a reference for the download
+    ref <- readr::read_lines(bib_file)
+    ref[1] <- paste0("@misc{GBIFRef,")
+    readr::write_lines(ref, bib_file)
+
 
 
     #-------unzip gbif data--------
 
     utils::unzip(fs::path(save_loc
-                          , paste0(info$key[[1]]
+                          , paste0(meta$key[[1]]
                                    , ".zip"
                                    )
                           )
                  , exdir = fs::path(save_loc
-                                    , info$key[[1]]
+                                    , meta$key[[1]]
                                     )
                  )
 
     rawGBIF <- data.table::fread(fs::path(save_loc
-                                          , info$key[[1]]
+                                          , meta$key[[1]]
                                           , "occurrence.txt"
                                           )
                                  ) %>%
@@ -166,7 +190,7 @@
       dplyr::filter(is.na(individualCount) | individualCount > 0) %>%
       dplyr::filter(is.na(organismQuantity) | organismQuantity > 0) %>%
       tibble::as_tibble() %>%
-      dplyr::mutate(doi = info$doi[[1]])
+      dplyr::mutate(doi = meta$doi[[1]])
 
     # What names to grab before writing results?
     if(is.null(data_map)) {
@@ -181,22 +205,24 @@
       unlist(., use.names=FALSE) %>%
       stats::na.omit()
 
+    dat <- rawGBIF %>%
+      dplyr::select(tidyselect::any_of(selectNames)) %>%
+      dplyr::filter(!is.na(eventDate)
+                    , !is.na(decimalLatitude)
+                    , !is.na(decimalLongitude)
+                    , !is.na(species)
+                    , species != ""
+                    )
+
     if(!is.null(out_file)) {
 
-      rio::export(rawGBIF %>%
-                    dplyr::select(tidyselect::any_of(selectNames)) %>%
-                    dplyr::filter(!is.na(eventDate)
-                                  , !is.na(decimalLatitude)
-                                  , !is.na(decimalLongitude)
-                                  , !is.na(species)
-                                  , species != ""
-                                  )
+      rio::export(dat
                   , out_file
                   )
 
     }
 
-    return(rawGBIF)
+    return(dat)
 
   }
 
