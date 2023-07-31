c7e399683312609c660a7ea8408f1c7ba31151f9
Nigel Willoughby <nigel.willougby@sa.gov.au>
2023-07-31 13:24:32 +0930

    `get_GBIF` updates

--- R/get_GBIF.r
+++ R/get_GBIF.r
@@ -13,19 +13,15 @@
 #' (or some higher group). These are the id numbers found in the GBIF backbone
 #' taxonomy. See
 #' [understanding GBIF taxonomic keys](https://discourse.gbif.org/t/understanding-gbif-taxonomic-keys-usagekey-taxonkey-specieskey/3045)
+#' @param aoi sf defining area of interest
+#' @param aoi_buf Numeric. Distance to buffer `aoi` via `sf::st_buffer` `dist`
+#' argument.
+#' @param min_year Numeric. gbif data will be filtered to >= min_year
 #' @param ... Other arguments passed to `rgbif::occ_download`
 #' @param save_file Character. File path into which to save outputs. If `null`
-#' results will be saved to `fs::path("out", "ds", "name", "name_raw.rds")`
-#' where `name` is the data source name.
-#' @param poly sf. Polygon defining area of interest for retrieving data.
-#' Acutally turned into `sf::st_bbox(poly)` before any `poly_buf` and converting
-#' to WKT using
-#' @param poly_buf Numeric. Distance to buffer `poly` via `sf::st_buffer` `dist`
-#' argument.
+#' results will be saved to `fs::path("out", "ds", "gbif", paste0(t"gbif_raw.rds")`.
 #' @param get_new Logical. If `FALSE` will attempt to load data from previously
 #' saved results.
-#' @param wait_time Time to wait between running `rgbif::occ_download_meta` to
-#' check status. Will keep trying while status == "RUNNING".
 #'
 #' @return Dataframe, `save_file` with occurence records, `gbif_data_ref.bib`
 #' (in the same directory as `save_file`) and full GBIF download.
@@ -38,119 +34,120 @@
 #' get_GBIF(taxon_key = 2474903, rgbif::pred("year", 1901))
 #'}
   get_GBIF <- function(taxon_key
+                       , aoi = NULL
+                       , aoi_buf = NULL
+                       , min_year = NULL
                        , ...
-                       , poly = NULL
-                       , poly_buf = 100000
                        , save_file = NULL
-                       , wait_time = 10
                        , get_new = FALSE
                        ) {
 
-    name <- "GBIF"
+    aoi_name <- deparse(substitute(aoi))
 
     if(is.null(save_file)) {
 
       save_file <- fs::path("out"
                             , "ds"
-                            , name
-                            , paste0(name
-                                     , "_"
-                                     , taxon_key
+                            , "GBIF"
+                            , taxon_key
+                            , paste0(taxon_key
+                                     , if(!is.null(aoi)) paste0("_"
+                                                                , aoi_name
+                                                                )
+                                     , if(!is.null(aoi_buf)) paste0("_"
+                                                                    , aoi_buf
+                                                                    )
                                      , "_raw.rds"
                                      )
                             )
 
     }
 
-    save_loc <- dirname(save_file)
+    save_dir <- dirname(save_file)
+
+    fs::dir_create(save_dir)
 
     get_new <- if(!file.exists(save_file)) TRUE else get_new
 
     if(get_new) {
 
-      if(wait_time < 10) wait_time = 10
+      if(!is.null(aoi)) {
 
-      if(!is.null(poly)) {
-
-        aoiWKT <- poly %>%
-          sf::st_buffer(poly_buf) %>%
+        aoiWKT <- aoi %>%
+          sf::st_buffer(aoi_buf) %>%
           sf::st_bbox() %>%
           sf::st_as_sfc() %>%
           sf::st_geometry() %>%
           sf::st_transform(crs = 4326) %>%
           sf::st_as_text()
 
-      }
-
-      temp <- rgbif::occ_download(rgbif::pred("taxonKey"
-                                              , taxon_key
-                                              )
-                                  , rgbif::pred("hasCoordinate"
-                                                , TRUE
-                                                )
-                                  , if(!is.null(poly)) rgbif::pred_within(aoiWKT)
-                                  , ...
-                                  )
-
-      meta <- rgbif::occ_download_meta(temp)
-
-      attempt <- 1
-
-      start_time <- Sys.time()
-
-      while(meta$status %in% c("PREPARING", "RUNNING")) {
-
-        cat(paste0("Status = ", meta$status, "\n"
-                   , "Attempt = ", attempt, "\n"
-                   , "Each wait time = ", wait_time, " seconds\n"
-                   , "Total time = ", Sys.time() - start_time, "\n"
-                   )
-            )
-
-        Sys.sleep(wait_time)
-
-        meta <- rgbif::occ_download_meta(temp)
-
-        attempt <- attempt + 1
-
-      }
-
-      if(meta$status != "SUCCEEDED") {
-
-        print(meta)
-
-        stop("Status not equal to 'SUCCEEDED'")
-
-      }
-
-      fs::dir_create(save_loc)
-
-      rgbif::occ_download_get(temp
-                              , path = save_loc
-                              , overwrite = TRUE
-                              )
-
-      utils::unzip(fs::path(save_loc
-                            , paste0(meta$key[[1]]
-                                     , ".zip"
-                                     )
-                            )
-                   , exdir = fs::path(save_loc
-                                      , meta$key[[1]]
+        gbif_download <- rgbif::occ_download(
+          rgbif::pred_and(rgbif::pred("HAS_GEOSPATIAL_ISSUE"
+                                      , FALSE
                                       )
-                   )
+                          , rgbif::pred("HAS_COORDINATE"
+                                        , TRUE
+                                        )
+                          , rgbif::pred("OCCURRENCE_STATUS"
+                                        , "PRESENT"
+                                        )
+                          , rgbif::pred_not(rgbif::pred_in("BASIS_OF_RECORD"
+                                                           , c("FOSSIL_SPECIMEN"
+                                                               , "LIVING_SPECIMEN"
+                                                               )
+                                                           )
+                                            )
+                          )
+          , rgbif::pred_in("taxonKey"
+                           , taxon_key
+                           )
+          , rgbif::pred_gte("year"
+                            , min_year
+                            )
+          , rgbif::pred_within(aoiWKT)
+          )
 
-      temp <- data.table::fread(fs::path(save_loc
-                                         , meta$key[[1]]
-                                         , "occurrence.txt"
-                                         )
-                                ) %>%
-        dtplyr::lazy_dt() %>%
-        dplyr::filter(is.na(occurrenceStatus) | occurrenceStatus != "ABSENT") %>%
-        dplyr::filter(is.na(individualCount) | individualCount > 0) %>%
-        dplyr::filter(is.na(organismQuantity) | organismQuantity > 0) %>%
+      } else {
+
+        gbif_download <- rgbif::occ_download(
+          rgbif::pred_and(rgbif::pred("HAS_GEOSPATIAL_ISSUE"
+                                      , FALSE
+                                      )
+                          , rgbif::pred("HAS_COORDINATE"
+                                        , TRUE
+                                        )
+                          , rgbif::pred("OCCURRENCE_STATUS"
+                                        , "PRESENT"
+                                        )
+                          , rgbif::pred_not(rgbif::pred_in("BASIS_OF_RECORD"
+                                                           , c("FOSSIL_SPECIMEN"
+                                                               , "LIVING_SPECIMEN"
+                                                               )
+                                                           )
+                                            )
+                          )
+          , rgbif::pred_in("taxonKey"
+                           , taxon_key
+                           )
+          , rgbif::pred_gte("year"
+                            , min_year
+                            )
+          )
+
+      }
+
+      rgbif::occ_download_wait(gbif_download)
+
+      meta <- rgbif::occ_download_meta(gbif_download)
+
+      gbif_download <- rgbif::occ_download_get(gbif_download
+                                               , path = save_dir
+                                               , overwrite = TRUE
+                                               )
+
+      temp <- rgbif::occ_download_import(gbif_download) %>%
         tibble::as_tibble() %>%
-        dplyr::mutate(doi = meta$doi[[1]])
+        dplyr::mutate(doi = meta$doi)
 
       rio::export(temp
                   , save_file
@@ -158,9 +155,8 @@
 
       # Make a reference for the download
 
-      bib_file <- fs::path(save_loc
-                           , paste0("gbif_data_ref_"
-                                    , taxon_key
+      bib_file <- fs::path(save_dir
+                           , paste0(meta$key
                                     , ".bib"
                                     )
                            )
@@ -174,9 +170,9 @@
 
       ref <- readr::read_lines(bib_file)
 
-      ref[1] <- paste0("@misc{GBIFRef_"
-                       , taxon_key
-                       , ","
+      ref[1] <- paste0("@misc{"
+                       , meta$key
+                       , "}"
                        )
 
       readr::write_lines(ref, bib_file)
