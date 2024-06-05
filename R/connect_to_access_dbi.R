a498bdbbefa00d2f7804ec1c1f3213b8f1494134
Acanthiza <nigel.willoughby@sa.gov.au>
2024-06-05 13:14:05 +0930

    `connect_to_access_dbi` as 64 bit replacement for `access_query_32`

---
+++ R/connect_to_access_dbi.R
@@ -0,0 +1,18 @@
+# Connect to access database via DBI function (to enable use with dbplyr package)
+# From icj's answer on https://stackoverflow.com/questions/46458092/connection-from-access-to-r-via-dbi/48390093#48390093
+
+connect_to_access_dbi <- function(db_file_path)  {
+  require(DBI)
+  # make sure that the file exists before attempting to connect
+  if (!file.exists(db_file_path)) {
+    stop("DB file does not exist at ", db_file_path)
+  }
+  # Assemble connection strings
+  dbq_string <- paste0("DBQ=", db_file_path)
+  driver_string <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+  db_connect_string <- paste0(driver_string, dbq_string)
+
+  myconn <- dbConnect(odbc::odbc(),
+                      .connection_string = db_connect_string)
+  return(myconn)
+}
