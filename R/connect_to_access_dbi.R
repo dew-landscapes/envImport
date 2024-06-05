7b16ff094e6aa266e648bc67f2354fafa1ca3ca9
Acanthiza <nigel.willoughby@sa.gov.au>
2024-06-05 13:16:03 +0930

    roxygen for connect_to_access_dbi

--- R/connect_to_access_dbi.R
+++ R/connect_to_access_dbi.R
@@ -1,6 +1,16 @@
-# Connect to access database via DBI function (to enable use with dbplyr package)
-# From icj's answer on https://stackoverflow.com/questions/46458092/connection-from-access-to-r-via-dbi/48390093#48390093
 
+
+#' Connect to access database via DBI function
+#'
+#' to enable use with dbplyr package. From icj's answer on
+#' https://stackoverflow.com/questions/46458092/connection-from-access-to-r-via-dbi/48390093#48390093
+#'
+#' @param db_file_path
+#'
+#' @return
+#' @export
+#'
+#' @examples
 connect_to_access_dbi <- function(db_file_path)  {
   require(DBI)
   # make sure that the file exists before attempting to connect
